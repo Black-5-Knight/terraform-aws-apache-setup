e8433d1079df31c17d614116612271f49558c575
Yousef Mohamed <144364402+Black-5-Knight@users.noreply.github.com>
2024-09-01 15:28:40 +0300

    Update ssh_key_pair.tf

--- terraform/ssh_key_pair.tf
+++ terraform/ssh_key_pair.tf
@@ -1,20 +1,4 @@
-# # Generate SSH key pair
-# resource "tls_private_key" "private_key" {
-#   algorithm = "RSA"
-#   rsa_bits   = 4096
-# }
 
-# # Create AWS key pair
-# resource "aws_key_pair" "key_pair" {
-#   key_name   = "day-5-key-pair"
-#   public_key = tls_private_key.private_key.public_key_openssh
-# }
-
-# # Output the private key
-# output "private_key_pem" {
-#   value = tls_private_key.private_key.private_key_pem
-#   sensitive = true
-# }
 resource "tls_private_key" "pk" {
   algorithm = "RSA"
   rsa_bits  = 4096
